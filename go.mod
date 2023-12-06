0712728c8e2ce6003a0ab62cc7c3ae99da43e537
CelepiYakup <135622873+CelepiYakup@users.noreply.github.com>
2023-12-06 15:13:49 +0300

    Fixing error in go files

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/CelepiYakup/golang-chat
 
 go 1.21.4
+
+require github.com/gorilla/websocket v1.5.0
--- pkg/websocket/pool.go
+++ pkg/websocket/pool.go
@@ -1,3 +1,9 @@
-
+package websocket
+type Pool struct {
+	Register   chan *Client
+	Unregister chan *Client
+	Clients    map[*Client]bool
+	Broadcast  chan Message
+}
 
 NewPool()
